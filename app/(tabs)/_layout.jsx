5e9fda32cec5919630290c2271268710386c646c
Kanif <kumbharkanif666@gmail.com>
2025-04-22 18:02:16 +0530

    feat: Add Hugeicons for tab bar icons and update dependencies

--- app/(tabs)/_layout.jsx
+++ app/(tabs)/_layout.jsx
@@ -1,14 +1,74 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 import {Tabs} from 'expo-router'
+import { HugeiconsIcon } from "@hugeicons/react-native";
+import {
+  Home12FreeIcons,
+  SpoonAndForkFreeIcons,
+  Chart03FreeIcons,
+  AccountSetting03FreeIcons,
+} from "@hugeicons/core-free-icons";
+import Colors from "./../../shared/Colors";
 
 export default function TabLayout() {
   return (
-    <Tabs>
-      <Tabs.Screen name="Home" />
-      <Tabs.Screen name="Meals" />
-      <Tabs.Screen name="Progress" />
-      <Tabs.Screen name="Profile" />
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: Colors.PRIMARY,
+      }}
+    >
+      <Tabs.Screen
+        name="Home"
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <HugeiconsIcon
+              icon={Home12FreeIcons}
+              size={size}
+              color={color}
+              strokeWidth={1.5}
+            />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="Meals"
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <HugeiconsIcon
+              icon={SpoonAndForkFreeIcons}
+              size={size}
+              color={color}
+              strokeWidth={1.5}
+            />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="Progress"
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <HugeiconsIcon
+              icon={Chart03FreeIcons}
+              size={size}
+              color={color}
+              strokeWidth={1.5}
+            />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="Profile"
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <HugeiconsIcon
+              icon={AccountSetting03FreeIcons}
+              size={size}
+              color={color}
+              strokeWidth={1.5}
+            />
+          ),
+        }}
+      />
     </Tabs>
-  )
+  );
 }
